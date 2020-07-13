b0ec59b49457a238f1e0740a156af1860ef0bd2d
sanglv4 <levansang8689@gmail.com>
2020-07-13 11:17:21 +0700

    [develop] add bootstrap sh file to github

---
+++ config-jenkins.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Jenkins
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update
+#echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+#sudo apt-get install oracle-java8-installer -y
+sudo apt-get install openjdk-8-jdk -y
+sudo apt-get install jenkins -y
+
+# Docker
+sudo apt-get update -y
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common gnupg-agent -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+
+# Azure CLI
+#echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+#sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+#sudo apt-get install apt-transport-https
+#sudo apt-get update && sudo apt-get install azure-cli
+curl -sL https://aka.ms/InstallAzureSLIDeb | sudo bash
+
+
+# Kubectl
+cd /tmp/
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+#sudo curl -kLO https://storage.googleapis.com/kubernetes-release/release/v1.8.0/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+sudo mv ./kubectl /usr/bin/kubectl
+# Configure access
+sudo usermod -aG docker jenkins
+sudo usermod -aG docker azureuser
+sudo touch /var/lib/jenkins/jenkins.install.InstallUtil.lastExecVersion
+sudo service jenkins restart
